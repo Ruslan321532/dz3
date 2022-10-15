7465122a1ff94f750ea6543948f64e4c473c0fc3
ruslan <ruslanburalkiev8@gmail.com>
2022-10-15 16:39:29 +0600

    ruslandz3t

---
+++ main.js
@@ -0,0 +1,67 @@
+
+
+
+// let a = [1, 3, 5, 8];
+// let b = [1, 3, 5, 8];
+// let c = [1, 3, 5, 7];
+// let d = [1, 3, 5, 8, 4];
+
+// function equal(x, y) {
+//   return x.length === y.length &&
+//          x.every((element, index) => y[index] === element);
+// }
+
+// console.log(equal(a, b));
+// console.log(equal(a, c));
+// console.log(equal(a, d));
+
+
+
+
+
+
+///////dz1--///////
+
+
+
+
+// const num1 = prompt("введите первое число")
+// const num2 = prompt("введите второе число")
+
+// function count (num1,num2) {
+//     if (num1 < num2) {
+//         alert(num1 + "Меньше " + num2)
+//     }else if(num1 > num2) {
+//         alert(num1 + "больше " + num2)
+//     }else {
+//         alert("Равны")
+//     }
+// }
+// count(num1,num2)
+
+/////-2/////
+
+// let arr1 = [1,2,3]
+// let arr2 = [1,2,3,4]
+
+// function twoMass (arr1,arr2) {
+//     if(arr1.length < arr2.length) {
+//         console.log(arr2.length);
+//     }else {
+//         console.log("ИДИ ДОМОЙ ");
+//     }
+// }
+// twoMass(arr1,arr2)
+
+
+//////3//////
+
+let k = "Hello"
+function countChar(k) {
+    if (k.length) {
+        console.log(k.length);
+    }else{
+        console.log("g");
+    }
+}
+countChar(k)
